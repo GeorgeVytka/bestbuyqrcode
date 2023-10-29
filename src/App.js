6d9d53a82fca18692930ca609b9866fe85a39cc4
John Doe <john@doe.org>
2023-10-28 20:17:25 -0400

    in the works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
+import {google,outlook, office365, yahoo, ics } from'calendar-link';
+import QRCode from 'react-qr-code';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 function App() {
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [selectedDate, setSelectedDate] = useState('');
+  const [qrCode, setQrCode] = useState("");
+
+// Set event as an object
+const events = {
+  title: "Buy more stuff at besstbuy",
+  description: "Be there!",
+  start: "2019-12-29 18:00:00 +0100",
+  duration: [3, "hour"],
+};
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+   
+
+    const tempEvent ={
+      title,description,"start":selectedDate
+    }
+  
+    setQrCode(ics(tempEvent))
+
+    console.log(qrCode);
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+     
+
+      <div className='create'>
+      
+  <h2>Best Buy Event Adder</h2>
+  <form onSubmit={handleSubmit}>
+    <label>Event title: </label>
+    <input type="text" 
+    value={title} 
+    onChange={(e) => setTitle(e.target.value)} required />
+
+    <label>Event description: </label>
+    <textarea required  
+    value={description}
+     onChange={(e) => setDescription(e.target.value)}> 
+     </textarea>
+
+
+<LocalizationProvider dateAdapter={AdapterDayjs}>
+      <DemoContainer components={['DateTimePicker']}>
+        <DateTimePicker label="pick a date for the event" onChange={(value) => setSelectedDate(value.$d)}
+        />
+      </DemoContainer>
+    </LocalizationProvider>
+    
+    <button>Add event: </button>
+  </form>
+      </div>
+    <div>
+
+        <QRCode size={200} bgColor='#0a4abf' fgColor='#f6eb16' value={qrCode}/>
+        { console.log("----", qrCode)}
+      </div>
     </div>
   );
 }
